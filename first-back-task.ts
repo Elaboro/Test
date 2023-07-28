a6f034fdf8c36af4748ccee6710be744cd4f5da0
Your Name <you@example.com>
2023-07-28 18:34:14 +0300

    first

--- first-back-task.ts
+++ first-back-task.ts
@@ -1,6 +1,28 @@
 import { Injectable } from "@nestjs/common";
 
+export type ActionTypeList = "start" | "connect" | "message" | "end";
+
 @Injectable()
 export class FirstBackTask {
-  static getResult() {}
+  static getResult(actions: ActionTypeList[]): number {
+    let result = 0;
+
+    const success = ["start", "connect", "message", "end"];
+
+    actions.forEach((v, i, a) => {
+      if(v = "start") {
+        if(i + 1 === undefined || i + 4 === undefined) return;
+
+        const condition = a.slice(i, i + 4);
+        const status = success.every((v, i) => v === condition[i]);
+        if(!status) return;
+
+        result++;
+      } else {
+        return;
+      }
+    });
+    
+    return result;
+  }
 }
--- index.ts
+++ index.ts
@@ -1,15 +1,29 @@
-import { FirstBackTask } from "./first-back-task";
+import { ActionTypeList, FirstBackTask } from "./first-back-task";
 import { SecondBackTask } from "./second-back-task";
 import { ThirdBackTask } from "./third-back-task";
 
 const main = () => {
-  const result1 = FirstBackTask.getResult();
+  const actions_list: ActionTypeList[] = [
+    "start", "connect", "message", "end"
+  ];
+  const result11 = FirstBackTask.getResult(actions_list);
+
+  const actions_list2: ActionTypeList[] = [
+    "start", "connect", "message", "end",
+    "start", "connect", "message", "end",
+    "start", "connect", "message",
+  ];
+  const result12 = FirstBackTask.getResult(actions_list2);
+
   const result2 = SecondBackTask.getResult();
   const result3 = ThirdBackTask.getResult();
 
   console.log(
     {
-      result1,
+      result1: {
+        result11,
+        result12,
+      },
       result2,
       result3,
     }
